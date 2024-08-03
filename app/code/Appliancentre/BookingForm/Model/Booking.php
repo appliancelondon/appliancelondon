7d4f7805bfd9d8029bf6a72aa3a8d4551ea29972
appliancelondon <sam@appliancentre.com>
2024-08-03 22:00:27 +0100

    Update Booking.php

--- app/code/Appliancentre/BookingForm/Model/Booking.php
+++ app/code/Appliancentre/BookingForm/Model/Booking.php
@@ -3,7 +3,9 @@ namespace Appliancentre\BookingForm\Model;
 
 use Magento\Framework\Model\AbstractModel;
 use Magento\Framework\DataObject\IdentityInterface;
-use Magento\Framework\Exception\LocalizedException;
+use Magento\Customer\Model\CustomerFactory;
+use Magento\Customer\Api\AccountManagementInterface;
+use Magento\Framework\Encryption\EncryptorInterface;
 
 class Booking extends AbstractModel implements IdentityInterface
 {
@@ -11,6 +13,26 @@ class Booking extends AbstractModel implements IdentityInterface
 
     protected $_cacheTag = 'appliancentre_bookingform_booking';
     protected $_eventPrefix = 'appliancentre_bookingform_booking';
+
+    protected $customerFactory;
+    protected $accountManagement;
+    protected $encryptor;
+
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        CustomerFactory $customerFactory,
+        AccountManagementInterface $accountManagement,
+        EncryptorInterface $encryptor,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        $this->customerFactory = $customerFactory;
+        $this->accountManagement = $accountManagement;
+        $this->encryptor = $encryptor;
+        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
+    }
 
     protected function _construct()
     {
@@ -22,59 +44,10 @@ class Booking extends AbstractModel implements IdentityInterface
         return [self::CACHE_TAG . '_' . $this->getId()];
     }
 
-    public function saveBooking($data)
+    public function getDefaultValues()
     {
-        $referenceNumber = $this->getNextReferenceNumber();
-        $data['reference_number'] = $referenceNumber;
-        
-        $this->_logger->info('Saving booking data: ' . json_encode($data));
-        
-        $isLandlord = isset($data['landlordAgent']) && $data['landlordAgent'] === 'yes';
-        
-        if ($isLandlord) {
-            $data['customer_name'] = $data['tenant_title'] . ' ' . $data['tenant_firstname'] . ' ' . $data['tenant_lastname'];
-            $data['customer_email'] = $data['tenant_email'];
-            $data['customer_phone'] = $data['tenant_phone'];
-            $data['customer_address'] = $this->formatAddress($data, 'tenant_');
-            
-            $data['landlord_name'] = $data['landlord_title'] . ' ' . $data['landlord_firstname'] . ' ' . $data['landlord_lastname'];
-            $data['landlord_email'] = $data['landlord_email'];
-            $data['landlord_phone'] = $data['landlord_phone'];
-            $data['landlord_address'] = $this->formatAddress($data, 'landlord_');
-        } else {
-            $data['customer_name'] = $data['title'] . ' ' . $data['firstname'] . ' ' . $data['lastname'];
-            $data['customer_email'] = $data['email'];
-            $data['customer_phone'] = $data['phone'];
-            $data['customer_address'] = $this->formatAddress($data);
-        }
-        
-        // Ensure these fields are saved
-        $data['appliance_type'] = $data['applianceType'];
-        $data['appliance_subtype'] = $data['applianceSubtype'];
-        $data['appliance_make'] = $data['applianceMake'];
-        $data['visit_date'] = $data['visitDate'];
-        $data['visit_time'] = $data['visitTime'];
-        $data['fault_description'] = $data['faultDescription'];
-        $data['model_number'] = $data['modelSerial'] ?? '';
-        $data['additional_info'] = $data['additionalInfo'] ?? '';
-        
-        $this->setData($data);
-        $this->save();
-        
-        $this->_logger->info('Saved booking data: ' . json_encode($this->getData()));
-
-        return $this->getId();
-    }
-
-    protected function formatAddress($data, $prefix = '')
-    {
-        $addressParts = [
-            $data[$prefix . 'address1'],
-            $data[$prefix . 'address2'] ?? '',
-            $data[$prefix . 'city'] ?? '',
-            $data[$prefix . 'postcode']
-        ];
-        return implode(', ', array_filter($addressParts));
+        $values = [];
+        return $values;
     }
 
     public function getNextReferenceNumber()
@@ -92,42 +65,84 @@ class Booking extends AbstractModel implements IdentityInterface
             return $counter;
         } catch (\Exception $e) {
             $connection->rollBack();
-            throw new LocalizedException(__('Unable to generate reference number. Please try again.'));
+            throw $e;
         }
     }
 
-    public function getReferenceNumber()
+    public function saveBooking($data)
     {
-        return $this->getData('reference_number');
+        $referenceNumber = $this->getNextReferenceNumber();
+        $data['reference_number'] = $referenceNumber;
+        
+        // Handle multiple appliances
+        if (isset($data['appliances']) && is_array($data['appliances'])) {
+            $appliances = [];
+            foreach ($data['appliances'] as $appliance) {
+                $appliances[] = [
+                    'type' => $appliance['applianceType'],
+                    'subtype' => $appliance['applianceSubtype'],
+                    'make' => $appliance['applianceMake']
+                ];
+            }
+            $data['appliances'] = json_encode($appliances);
+        }
+
+        $this->setData($data);
+        $this->save();
+
+        // Create customer account
+        $this->createCustomerAccount($data);
+
+        return $this->getId();
     }
 
-    public function getApplianceMake()
+    public function isValidPostcode($postcode)
     {
-        return $this->getData('appliance_make');
+        // Implement your postcode validation logic here
+        // This is a simple example, you should replace it with your actual validation
+        $validPostcodes = ['N1', 'N2', 'N3', 'N4', 'N5']; // Add your valid postcodes here
+        $postcode = strtoupper(substr($postcode, 0, 2)); // Get first two characters of postcode
+        return in_array($postcode, $validPostcodes);
     }
 
-    public function getApplianceType()
+    public function getSubmissionCount()
     {
-        return $this->getData('appliance_type');
+        $collection = $this->getCollection();
+        return $collection->getSize();
     }
 
-    public function getApplianceSubtype()
+    public function calculateQuoteTotal($subTotal, $classFieldRadio)
     {
-        return $this->getData('appliance_subtype');
+        // Implement your quote calculation logic here
+        return $subTotal + $classFieldRadio;
     }
 
-    public function getModelNumber()
+    protected function createCustomerAccount($data)
     {
-        return $this->getData('model_number');
+        try {
+            $customer = $this->customerFactory->create();
+            $customer->setWebsiteId(1); // Set the appropriate website ID
+            $customer->setEmail($data['email']);
+            $customer->setFirstname($data['firstname']);
+            $customer->setLastname($data['lastname']);
+            
+            $password = $this->generatePassword();
+            $hash = $this->encryptor->getHash($password, true);
+            $customer->setPasswordHash($hash);
+
+            $customer->save();
+            
+            // You might want to send an email to the customer with their account details here
+            
+            return $customer->getId();
+        } catch (\Exception $e) {
+            // Handle the exception (e.g., log it or throw a custom exception)
+            $this->_logger->critical('Error creating customer account: ' . $e->getMessage());
+        }
     }
 
-    public function getVisitDate()
+    protected function generatePassword()
     {
-        return $this->getData('visit_date');
-    }
-
-    public function getVisitTime()
-    {
-        return $this->getData('visit_time');
+        return substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'), 0, 12);
     }
 }
