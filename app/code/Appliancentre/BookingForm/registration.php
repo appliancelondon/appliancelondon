73955a123ed612d57e47ef6d53a7a88354c3a888
appliancelondon <sam@appliancentre.com>
2024-08-03 19:43:01 +0100

    Delete app/code/Appliancentre directory

--- app/code/Appliancentre/BookingForm/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Copyright © Appliancentre All rights reserved.
- * See COPYING.txt for license details.
- */
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Appliancentre_BookingForm', __DIR__);
